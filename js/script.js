0ba3ff60c52f5133eee369a3c32d47aa5705bc14
Aiyegbajeje Yusuf <refsilva@yahoo.com>
2022-12-13 18:37:55 +0100

    latest444

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 14, 2022 18:02:00").getTime();
+var countDownDate = new Date("Dec 15, 2022 18:02:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
